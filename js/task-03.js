e69f21f8894ad75a4dee0d221d7e105a6cefebbb
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2020-01-08 01:43:46 +0200

    Module5

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
