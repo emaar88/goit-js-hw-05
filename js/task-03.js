3aa0b5a773ad30215f9d9cabf9e0925d3d75878b
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2020-01-08 13:55:55 +0200

    Module5

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,43 @@
 'use strict';
+
+
+class Storage{
+  constructor([...items]) {
+  this.items = items;
+  }
+  getItems()
+  {
+      return this.items;
+  }
+  addItem(item)
+  {
+    
+    this.items.push(item);
+  }
+  removeItem(item)
+  {
+    let findItemToRemove = 0;
+    for (findItemToRemove; findItemToRemove<this.items.length; findItemToRemove++) {
+      if (item ===  this.items[findItemToRemove])
+      {
+      this.items.splice(findItemToRemove, 1);
+      }
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,34 @@
 'use strict';
+class StringBuilder
+{
+    constructor(_value)
+    {
+        this._value = _value;
+    }
+    get value()
+    {
+        return this._value;
+    }
+    append(string)
+    {
+        return this._value = this._value.concat(string);
+    }
+    prepend(string)
+    {
+        return this._value = string.concat(this._value)
+    }
+    pad(string)
+    {
+        return this._value = string.concat(this._value).concat(string)
+    }
+}
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
